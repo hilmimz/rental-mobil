19a9c85672a13ada76078f5a612fcb390851ec34
maulanadaffaay <maulanadaffa@student.uns.ac.id>
2022-06-09 20:01:43 +0700

    Menambahkan CRUD Pelanggan

---
+++ resources/views/dashboard/pelanggan/create.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.dashboard')
+
+@section('upper_links')
+    @include('partials.datatables_upper_links')
+@endsection
+
+@section('content')
+        <!-- Table -->
+        <div class="container mt-3 d-flex justify-content-center">
+            <div class="card rounded-3" style="width: 1100px;">
+                <div class="card-header mw-100 h-100 text-center btn-secondary rounded-3 shadow-lg">
+                    <h2 class="mt-0 mb-0" style="color: #fff; font-size: 22px;"><b>Silahkan Isi Data Berikut</b></h2>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('pelanggan.store') }}">
+                        @csrf
+                        {{-- <input type="hidden" value="{{ $nomor }}" name="nomor"> --}}
+                        <div class="mb-3 mt-3 row">
+                            <label for="" class="col-sm-2 col-form-label">NIK :</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="nik" class="form-control" id="nik" autocomplete="off" placeholder="Masukkan NIK">
+                            </div>
+                        </div>
+                        <div class="mb-3 mt-4 row">
+                            <label for="" class="col-sm-2 col-form-label">Nama :</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="nama" class="form-control" id="nama"  placeholder="Masukkan Nama" autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="mb-3 mt-4 row">
+                            <label for="" class="col-sm-2 col-form-label">Gender :</label>
+                            <div class="col-sm-10">
+                            <select class="form-select" name="jenis_kelamin" id="jenis_kelamin" aria-label="Default select example">
+                                    <option selected>Pilih...</option>
+                                    <option value="L">L</option>
+                                    <option value="P">P</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="mb-3 mt-4 row">
+                            <label for="" class="col-sm-2 col-form-label">Tgl Lahir :</label>
+                            <div class="col-sm-10">
+                                <input type="date" name="tgl_lahir" class="form-control" id="tgl_lahir"  placeholder="Masukkan Tanggal Lahir" autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="mb-3 mt-4 row">
+                            <label for="" class="col-sm-2 col-form-label">Alamat :</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="alamat" class="form-control" id="alamat"  placeholder="Masukkan Alamat" autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="mb-3 mt-4 row">
+                            <label for="" class="col-sm-2 col-form-label">No Telp :</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="no_telepon" class="form-control" id="no_telepon"  placeholder="Masukkan No. Telp" autocomplete="off">
+                            </div>
+                        </div>
+    
+                        <div class="mb-3 row mt-4">
+                            <div class="col">
+                                <button type="submit" name="aksi" value="tambah" class="btn btn-primary">
+                                    Submit
+                                </button>
+                                <a href="{{ route('pelanggan.index') }}" type="button" class="btn btn-danger">
+                                    Cancel 
+                                </a>
+                            </div>
+                        </div>
+                    </form>                
+                </div>
+            </div>
+        </div>
+        <!-- End Table -->
+
+@endsection
+
+@section('bottom_links')
+    @include('partials.datatables_bottom_links')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArmadaController;
 use App\Http\Controllers\MerkController;
+use App\Http\Controllers\PelangganController;
 
 //Test
 /*
@@ -24,4 +25,6 @@ Route::get('/armada/', [ArmadaController::class, 'index']);
 Route::get('/armada/create', [ArmadaController::class, 'create']);
 
 Route::resource('merk', MerkController::class);
+Route::resource('pelanggan', PelangganController::class);
+
 // tambahin
