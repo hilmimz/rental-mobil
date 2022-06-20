e33c897af6f300cbddb224788456e24e6713d360
Hilmi <hilmimuzhaffar15@gmail.com>
2022-06-20 08:15:32 +0700

    Menambahkan migration untuk user dan mengedit route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ArmadaController;
 use App\Http\Controllers\MerkController;
 use App\Http\Controllers\PembayaranController;
